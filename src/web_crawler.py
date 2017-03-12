412622ea282101c80484ad4c7477920357e062b6
Kit Lawes <kitlawes@hotmail.com>
2017-03-12 14:38:52 +0000

    Implement get_links in web_crawler

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -7,3 +7,12 @@ class WebCrawler:
         for line in urllib2.urlopen(url):
             html.append(line)
         return html
+
+    def get_links(self, html):
+        links = []
+        for line in html:
+            if "href=\"" in line:
+                start = line.index("href=\"") + len("href=\"")
+                end = line.index("\"", start)
+                links.append(line[start: end])
+        return links
