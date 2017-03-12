7a00173c1c2498255f4119ad3beda5b1046f4b75
Kit Lawes <kitlawes@hotmail.com>
2017-03-12 15:28:12 +0000

    Implement get_same_subdomain_addresses_from_links in web_crawler

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -17,5 +17,11 @@ class WebCrawler:
                 links.append(line[start: end])
         return links
 
-    def get_same_subdomain_addresses_from_links(self, links, subdomain):
-        pass
+    def get_same_subdomain_addresses_from_links(self, subdomain, links):
+        same_subdomain_addresses = []
+        for link in links:
+            if link.startswith("/"):
+                same_subdomain_addresses.append(subdomain.rstrip('/') + link)
+            if link.startswith(subdomain):
+                same_subdomain_addresses.append(link)
+        return same_subdomain_addresses
